080ce7e84e79a7d4eca93e391aa76b215b0172ea
colezhichen <37854437+colezhichen@users.noreply.github.com>
2023-04-23 18:55:33 -0500

    basic components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,11 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-augular-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-augular-app');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
