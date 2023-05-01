3c245bc5e4aacdb70ea335f0606995dfeb935ef1
colezhichen <37854437+colezhichen@users.noreply.github.com>
2023-05-01 00:45:40 -0500

    Added the skeloton of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showParagrah = false;
-  log = [];
-  numberClicked = 0;
- 
-  onToggleDetails() {
-    this.showParagrah = !this.showParagrah;
-    // this.log.push(this.log.length + 1);
-    this.log.push(new Date());
-  }
+
 }
